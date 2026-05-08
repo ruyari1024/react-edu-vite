9f4977f06823edffde93eba0d44b8024716db98c
ruyari1024 <ruyari1024@gmail.com>
2026-05-08 19:15:07 +0900

    Reapply "project intailger"
    This reverts commit 2cd2747fa3b5ee837d695535e4b4355c34fc91b6.

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import { useEffect } from 'react';
+import './App.css'
+import type { listItemType } from './components/ListItem/ListItem'
+import ListItem from './components/ListItem/ListItem'
+import { useFieldArray, useForm } from 'react-hook-form';
+
+interface formType {
+  phoneticList : listItemType[];
+  newItem: string
+}
+
+function App() {
+
+  const {control, register, getValues, resetField, watch} = useForm<formType>({
+    defaultValues: {
+      phoneticList: [{title: 'alpha', id: 1}
+    , {title: 'bravo', id: 2}
+    , {title: 'charlie', id: 3}
+    , {title: 'delta', id: 4}
+    , {title: 'echo', id: 5}
+    , {title: 'foxtrot', id: 6}],
+    newItem: ''
+    }
+  })
+
+  const {fields, append, remove} = useFieldArray({control, name: 'phoneticList'})
+  
+  const handleAddList = () => {
+    const value = getValues('newItem')
+    if(value) {
+      append({title: value, id: fields.length})
+      resetField('newItem')
+    }
+  }
+  
+  const handleDeleteList = (target:number) => {
+    remove(target)
+  }
+
+  const handleEnterKey = (e:any) => {
+    if(e.key === 'Enter') {
+      handleAddList()
+    }
+  }
+
+  return (
+    <div className='body'>
+      <div className='list-contents'>
+        {
+        fields.map((item, index) => {
+          return (
+            <ListItem item={item} index={index} handleDelete={() => {handleDeleteList(index)}} key={item.id}></ListItem>
+          )
+        })
+        }
+      </div>
+
+      <div className='input-field-container'>
+        <input className='input' {...register("newItem")} onKeyDown={handleEnterKey}></input> 
+        <button className='plus-btn' onClick={handleAddList}>+</button>
+      </div>
+    </div>
+  )
+}
+
+
+export default App
